bd05fc59721d7b7180d29b00bb85b2e0e47c02ec
Nour <nourbouzaffara@hotmail.fr>
2018-12-09 16:11:04 +0100

    list chat rooms

---
+++ src/components/RoomList.js
@@ -0,0 +1,33 @@
+
+import React, { Component } from 'react';
+
+class room-list extends React.Component {
+  constructor(props){
+           super(props);
+           this.state = {
+             rooms: []
+           }
+           this.roomsRef = this.props.firebase.database().ref('rooms');
+         }
+
+        componentDidMount() {
+              this.roomsRef.on('child_added', snapshot => {
+              const room = snapshot.val();
+              room.key = snapshot.key;
+              this.setState({ rooms: this.state.rooms.concat( room ) })
+              });
+            }
+
+        render() {
+          return (
+            this.state.rooms.map( (room, index) =>
+                      <div key={index} >{this.state.rooms.room}</div>
+               )
+          );
+        }
+      }
+      ReactDOM.render(
+              <room-list/>,document.getElementById('app')
+            );
+
+export default RoomList;
