8540338c23b88609a41c10b4340368ebddec516c
Nour <nourbouzaffara@hotmail.fr>
2018-12-24 13:45:38 +0100

    new rooms enabled

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,19 +5,35 @@ class RoomList extends Component {
     super(props);
 
     this.state = {
-      rooms: []
+      rooms: [],
+      newRoomTitle: ''
     };
 
     this.roomsRef = this.props.firebase.database().ref('room');
-  }
+}
 
-  componentDidMount() {
+componentDidMount() {
     this.roomsRef.on('child_added', snapshot => {
       const room = snapshot.val();
       room.key = snapshot.key;
       this.setState({ rooms: this.state.rooms.concat( room ) })
     });
-  }
+}
+
+handleChange(event) {
+     this.setState({newRoomTitle: event.target.value});
+}
+
+createRoom(event){
+  event.preventDefault();
+  const newRoomTitle = this.state.newRoomTitle;
+  this.roomsRef.push({
+    name: newRoomTitle
+  });
+  const emptyString = '';
+  this.setState({ newRoomTitle: emptyString });
+
+}
 
   render() {
       return(
@@ -28,6 +44,17 @@ class RoomList extends Component {
               <div key={ index }><h3>{ room.name }</h3></div>
             )
           }
+          <form
+          onSubmit={ (event) => this.createRoom(event)}>
+          <label for="{ this.state.newRoomTitle }">
+          </label>
+          <input
+          type = "text"
+          value ={ this.state.newRoomTitle }
+          onChange = { (event) => this.handleChange(event) }
+          />
+          <button type="submit"> Create Room </button>
+          </form>
         </nav>
       );
     }
